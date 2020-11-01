c2f61af98ac22ccd5417728f41d5bc1da146cdde
Yuriy Petrukhin <8883339@mail.ru>
2020-11-01 22:29:25 +0300

    Task01 test debug

--- homework1/sample_project/tests/test_calculator.py
+++ homework1/sample_project/tests/test_calculator.py
@@ -5,13 +5,16 @@ from calc import check_power_of_2
 
 @pytest.mark.parametrize(
     ["value", "expected_result"],
-    [
-        (65536, True),
+    {
+         (65536, True),
         (12, False),
-        (2, True),
+        [2, True],
         (1, False),
-    ],
+    },
 )
+
+
+
 def test_power_of_2(value: int, expected_result: bool):
     actual_result = check_power_of_2(value)
 
