4f906c46f7bd1193a413720ba9d8b112cdf7bad8
Yuriy Petrukhin <8883339@mail.ru>
2020-11-03 20:47:38 +0300

    add TypeError tests

--- homework1/sample_project/calculator/calc.py
+++ homework1/sample_project/calculator/calc.py
@@ -1,4 +1,6 @@
 def check_power_of_2(a: int) -> bool:
+    if type(a) not in [int]:
+        raise TypeError("a must be integer")
     if a < 2:
         return False
     return not (bool(a & (a - 1)))
--- homework1/sample_project/tests/test_calculator.py
+++ homework1/sample_project/tests/test_calculator.py
@@ -3,8 +3,27 @@ from calc import check_power_of_2
 
 
 @pytest.mark.parametrize(
-    ["value", "expected_result"], {(65536, True), (12, False), [2, True], (1, False)}
+    ["value", "expected_result"], {(65536, True), (12, False), (2, True), (-10, False)}
 )
 def test_power_of_2(value: int, expected_result: bool):
     actual_result = check_power_of_2(value)
+
     assert actual_result == expected_result
+
+
+def test_throw_exceptions_when_value_is_bool():
+    with pytest.raises(TypeError) as ex:
+        check_power_of_2(True)
+    assert "a must be integer" in str(ex.value)
+
+
+def test_throw_exceptions_when_value_is_string():
+    with pytest.raises(TypeError) as ex:
+        check_power_of_2("kawabanga")
+    assert "a must be integer" in str(ex.value)
+
+
+def test_throw_exceptions_when_value_is_float():
+    with pytest.raises(TypeError) as ex:
+        check_power_of_2(2.0)
+    assert "a must be integer" in str(ex.value)
