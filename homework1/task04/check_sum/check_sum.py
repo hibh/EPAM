30f3cb0a7a9bf9faee34c61aaebf320d5b3202d6
Yuriy Petrukhin <8883339@mail.ru>
2020-11-10 23:46:32 +0300

    Correct hw task 4, swap algorithmic decision on cartesian product)

--- homework1/task04/check_sum/check_sum.py
+++ homework1/task04/check_sum/check_sum.py
@@ -6,15 +6,15 @@ Given four lists A, B, C, D of integer values,
 
 We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
 """
+import itertools
 from typing import List
 
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
     count = 0
-    for i in a:
-        for j in b:
-            for k in c:
-                for m in d:
-                    if i + j + k + m == 0:
-                        count += 1
+    cartesian_product = itertools.product(a, b, c, d)
+    cartesian_list = list(cartesian_product)
+    for each_list in cartesian_list:
+        if sum(each_list) == 0:
+            count += 1
     return count
