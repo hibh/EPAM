3544bf40e8dc62f0c45f676aaebccaf4a514d8b5
Yuriy Petrukhin <8883339@mail.ru>
2020-11-10 22:31:42 +0300

    HW02 Task3 add body of function  combinatons

--- homework1/sample_project/calculator/calc.py
+++ homework1/sample_project/calculator/calc.py
@@ -1,6 +1,6 @@
 def check_power_of_2(a: int) -> bool:
-    if type(a) not in [int]:
-        raise TypeError("a must be integer")
-    if a < 2:
+    if not isinstance(a, int):
+        raise TypeError("Value must be int")
+    if a < 1:
         return False
-    return not (bool(a & (a - 1)))
+    return not (a & (a - 1))
--- homework1/sample_project/tests/test_calculator.py
+++ homework1/sample_project/tests/test_calculator.py
@@ -9,21 +9,3 @@ def test_power_of_2(value: int, expected_result: bool):
     actual_result = check_power_of_2(value)
 
     assert actual_result == expected_result
-
-
-def test_throw_exceptions_when_value_is_bool():
-    with pytest.raises(TypeError) as ex:
-        check_power_of_2(True)
-    assert "a must be integer" in str(ex.value)
-
-
-def test_throw_exceptions_when_value_is_string():
-    with pytest.raises(TypeError) as ex:
-        check_power_of_2("kawabanga")
-    assert "a must be integer" in str(ex.value)
-
-
-def test_throw_exceptions_when_value_is_float():
-    with pytest.raises(TypeError) as ex:
-        check_power_of_2(2.0)
-    assert "a must be integer" in str(ex.value)
